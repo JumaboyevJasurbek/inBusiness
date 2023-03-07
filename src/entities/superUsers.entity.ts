e9d8a94b163306eda72b8eaad713b635116dcf71
JumaboyevJasurbek <100076086+JumaboyevJasurbek@users.noreply.github.com>
2023-03-07 21:27:11 +0500

    deleted migrations

--- src/entities/superUsers.entity.ts
+++ src/entities/superUsers.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity({
-  name: "superUsers",
+  name: "super_users",
 })
 export class SuperUsersEntity {
   @PrimaryGeneratedColumn("uuid")
