cf4fbd40ee52f18250a53413d139d735ffe08b4e
JumaboyevJasurbek <100076086+JumaboyevJasurbek@users.noreply.github.com>
2023-03-09 14:25:15 +0500

    ormconfig fixed

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -6,11 +6,11 @@ dotenv.config()
 
 export const dataSource = new DataSource({
   type: "postgres",
-  host: "tiny.db.elephantsql.com",
+  host: "localhost",
   port: 5432,
-  password: "107DAW_qYYEqxBB_W_4GtKOgcITf5Mvf",
-  username: "xxhmrfmz",
-  database: "xxhmrfmz",
+  password: "1sU*DtM&9RfB",
+  username: "postgres",
+  database: "inbusiness",
   entities: [path.join(__dirname, "..", "entities", "*.entity.{ts,js}")],
   migrations: [path.join(__dirname, "..", "migrations", "**/*.{ts,js}")],
   logging: true,
